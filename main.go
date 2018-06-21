8bc0d0cc85604776bb67a40be7988e47174bc3bb
Jonah George <jonah@jonahgeorge.com>
2018-06-20 21:13:49 -0700

    Make run on Heroku

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/subosito/gotenv"
 )
@@ -10,10 +11,15 @@ import (
 func main() {
 	gotenv.Load(".env")
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	http.HandleFunc("/webhook", WebHookHandler)
 	http.HandleFunc("/kyler", kylerHandler)
 	http.HandleFunc("/", HelloWorldHandler)
 
-	fmt.Println("Listening on localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	fmt.Printf("Listening on :%s\n", port)
+	http.ListenAndServe(":"+port, nil)
 }
