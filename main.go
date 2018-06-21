f3ddf8e89ec8773bd1444cfe8bacb4bf1a8a6fa1
Jonah George <jonah@jonahgeorge.com>
2018-06-20 21:03:17 -0700

    fix

--- main.go
+++ main.go
@@ -2,17 +2,15 @@ package main
 
 import (
 	"fmt"
-  "log"
-	"os"
-	"github.com/subosito/gotenv"
-  "net/http"
-)
+	"net/http"
 
+	"github.com/subosito/gotenv"
+)
 
 func main() {
 	gotenv.Load(".env")
 
-  http.HandleFunc("/webhook", WebHookHandler)
+	http.HandleFunc("/webhook", WebHookHandler)
 	http.HandleFunc("/kyler", kylerHandler)
 	http.HandleFunc("/", HelloWorldHandler)
 
